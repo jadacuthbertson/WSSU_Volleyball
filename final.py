1e7e7ffe88c6d414a2b9771cc660b410f808390b
jadacuthbertson <150089211+jadacuthbertson@users.noreply.github.com>
2023-12-04 19:26:30 -0500

    Update final.py

--- final.py
+++ final.py
@@ -2,6 +2,14 @@ import altair as alt
 import pandas as pd
 import streamlit as st
 
-volleyball_df = pd.read_csv("WSSU Volleyball Data.csv")
-volleyball_df
 
+st.title("2023-24 Winston-Salem State University Women's Volleyball")
+
+volleyball_df = pd.read_csv("WSSU Volleyball Data.csv")
+st.dataframe(volleyball_df)
+
+scatter = alt.Chart(volleyball_df).mark_point().encode(
+    alt.X("Position", title = "Position"),
+    alt.Y("Attack Errors", title = "Attack Errors")
+)
+st.altair_chart(scatter, use_container_width=True)
